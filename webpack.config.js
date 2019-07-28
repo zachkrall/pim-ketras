263f907272e2b68b4fbaf89e42a3ed8b3934dc81
Glitch (excited-pot) <none>
2019-07-28 23:46:36 +0000

    🎇🌖 Checkpoint ./public/client.js:5775005/1170 ./src/app.js:5775005/227

---
+++ webpack.config.js
@@ -0,0 +1,41 @@
+const path = require('path');
+const webpack = require('webpack');
+
+module.exports = {
+  mode: "development",
+  entry: path.resolve(__dirname, "src", "app.js"),
+  output: {
+    path: path.resolve(__dirname, "public"),
+    filename: "client.js",
+    publicPath: "/public/"
+  },
+  module: {
+    rules: [
+      {
+        test: /\.js?$/,
+        exclude: /node_modules/,
+        use: {
+          loader: "babel-loader"
+        }
+      },
+      {
+        test: /\.css$/,
+        loaders: ['style-loader','css-loader']
+      }
+    ]
+  },
+  devServer: {
+    contentBase: path.join(__dirname),
+    compress: true,
+    port: 3000,
+    hot: true,
+    writeToDisk: true,
+    historyApiFallback: true
+  },
+  plugins: [
+    new webpack.HotModuleReplacementPlugin(), // Enable hot reloading
+  ],
+  node: {
+    fs: 'empty'
+  }
+};
