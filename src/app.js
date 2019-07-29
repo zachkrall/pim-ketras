719a0e9ecdf49891382dd2e1385e84ff19929a56
Glitch (excited-pot) <none>
2019-07-29 00:07:38 +0000

    🐸🍬 Checkpoint ./src/app.js:274611/810 ./package.json:274611/2437

--- src/app.js
+++ src/app.js
@@ -1,9 +1,31 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
+import p5 from 'p5';
+
 export default class App extends React.Component{
   
+  componentWillMount(){
+  }
+  
   render(){
+    
+    const sketch = (p) =>{
+    
+      const setup = p => {
+        p.createCanvas(400,400);
+        p.background(0);
+      }
+
+      const draw = p => {
+        p.fill(255,0,0);
+        p.rect(10,10,10,10);
+      }
+    
+    }
+    
+    const p5sketch = new p5(sketch);
+    
     return (
       <h1>Hello, World! :(</h1>
     )
